e3fe08e3ccdc355c15ae04c07a0894d493625caa
kennethchu-statcan <kenneth.chu@canada.ca>
2020-01-01 15:16:03 -0500

    updated: installRequiredPkgs.R (added installation of BiocManager, ComplexHeatmap)

--- code/installRequiredPkgs.R
+++ code/installRequiredPkgs.R
@@ -1,7 +1,8 @@
 
 installRequiredPkgs <- function(
-    temp.RLib         = "RLib",
-    packages.required = c("ggplot2","gplots","text2vec","xml2","stopwords","dplyr","tidyr")
+    temp.RLib     = "RLib",
+    packages.Bioc = c("ComplexHeatmap"),
+    packages.CRAN = c("ggplot2","gplots","text2vec","xml2","stopwords","dplyr","tidyr")
     ) {
 
     cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
@@ -19,16 +20,25 @@ installRequiredPkgs <- function(
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     # determine required but not-yet-installed R packages
-    packages.to.be.installed <- setdiff(
-        packages.required,
+
+    packages.to.be.installed.Bioc <- setdiff(
+        packages.Bioc,
         installed.packages()[,"Package"]
         );
-
-    cat("\n# packages.to.be.installed\n");
-    print( packages.to.be.installed   );
-
+    
+    cat("\n# packages.to.be.installed.Bioc\n");
+    print(   packages.to.be.installed.Bioc   );
+    
+    packages.to.be.installed.CRAN <- setdiff(
+        packages.CRAN,
+        installed.packages()[,"Package"]
+        );
+    
+    cat("\n# packages.to.be.installed.CRAN\n");
+    print(   packages.to.be.installed.CRAN   );
+    
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    if ( length(packages.to.be.installed) > 0 ) {
+    if ( max(length(packages.to.be.installed.Bioc),length(packages.to.be.installed.CRAN)) > 0 ) {
 
         # get URL of an active CRAN mirror
         CRANmirrors   <- getCRANmirrors();
@@ -40,21 +50,43 @@ installRequiredPkgs <- function(
         } else if (nrow(CRANmirrors) > 0) {
             myRepoURL <- CRANmirrors[1,"URL"];
         } else {
+            cat("\nERROR: Unable to find CRAN mirrors ...\n");
             quit(save = "no");
-            }
+        }
 
         cat(paste0("\n# myRepoURL = ",myRepoURL,"\n"));
 
-        cat("\n# installation of packages starts ...\n");
-        # install required but not-yet-installed R packages to temporary R library
-        install.packages(
-            pkgs         = packages.to.be.installed,
-            lib          = temp.RLib,
-            repos        = myRepoURL,
-            dependencies = TRUE # c("Depends", "Imports", "LinkingTo", "Suggests")
-            );
-        cat("\n# installation of packages completed ...\n");
+        if ( length(packages.to.be.installed.Bioc) > 0 ) {
+            cat("\n##### installation of BiocManager starts ...\n");
+            install.packages(
+                pkgs         = c("BiocManager"),
+                lib          = temp.RLib,
+                repos        = myRepoURL,
+                dependencies = TRUE # c("Depends", "Imports", "LinkingTo", "Suggests")
+                );
+            cat("\n##### installation of BiocManager complete ...\n");
+            library(package = "BiocManager", character.only = TRUE, lib.loc = temp.RLib);
+            cat("\n##### installation of Bioconductor packages starts ...\n");
+            BiocManager::install(
+                pkgs         = packages.to.be.installed.Bioc,
+                lib          = temp.RLib,
+                dependencies = TRUE
+                );
+            cat("\n##### installation of Bioconductor packages complete ...\n");
+            }
 
+        if ( length(packages.to.be.installed.CRAN) > 0 ) {
+            cat("\n# installation of packages starts ...\n");
+            # install required but not-yet-installed R packages to temporary R library
+            install.packages(
+                pkgs         = packages.to.be.installed.CRAN,
+                lib          = temp.RLib,
+                repos        = myRepoURL,
+                dependencies = TRUE # c("Depends", "Imports", "LinkingTo", "Suggests")
+                );
+            cat("\n# installation of packages completed ...\n");
+            }
+        
         }
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
@@ -63,4 +95,3 @@ installRequiredPkgs <- function(
     return( NULL );
 
     }
-
