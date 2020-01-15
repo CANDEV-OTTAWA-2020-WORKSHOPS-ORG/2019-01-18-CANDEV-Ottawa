9b9f5fa7ce8a6080a89802d21e84d23a27038c93
kennethchu-statcan <58754269+kennethchu-statcan@users.noreply.github.com>
2020-01-15 17:40:09 -0500

    added: runall.bat

---
+++ runall.bat
@@ -0,0 +1,30 @@
+
+@echo off
+setlocal
+
+:: ########################################################
+:: get folder containing this script
+set thisFolder=%~dp0
+set thisFolder=%thisFolder:~0,-1%
+
+:: get task name
+for %%f in (%thisFolder%) do set task=%%~nf
+
+:: ########################################################
+:: defining folder names
+set codeDIR=%thisFolder%\code
+set dataDIR=%thisFolder%\data
+set outROOT=%thisFolder%\output
+
+:: make output root directory if not already exists
+if not exist %outROOT%\code mkdir %outROOT%\code
+
+:: copy this batch script to output root
+copy %0 %outROOT%\code
+:: copy code folder to output root
+xcopy /E /I /Y %codeDIR% %outROOT%\code
+
+:: ########################################################
+:: R command
+set PATH=F:\work\software\R\instances\R-3.6.2\bin;%PATH%
+Rscript %codeDIR%\runall.R %dataDIR% %codeDIR% %outROOT% 1> %outROOT%\stdout.R.runall 2> %outROOT%\stderr.R.runall
